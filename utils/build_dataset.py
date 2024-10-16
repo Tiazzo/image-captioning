3ca217627d7575a8c8a1a902906498f331e9802c
whiitex <pasquale03.bianco@gmail.com>
2024-10-16 13:35:18 +0200

    fix: minor

--- utils/build_dataset.py
+++ utils/build_dataset.py
@@ -15,17 +15,18 @@ transform = Compose([
 
 def build_datasets(BATCH_SIZE=BATCH_SIZE, IMG_SIZE=IMG_SIZE, transform=transform):
 
+    # Datasets
     train_dataset = ImageCaptionDataset("../data/train", "../data/train/captions_train.txt", transform)
     val_dataset = ImageCaptionDataset("../data/val", "../data/val/captions_val.txt", transform)
     test_dataset = ImageCaptionDataset("../data/test", "../data/test/captions_test.txt", transform)
-
     small_train_dataset = ImageCaptionDataset("../data/train_small", "../data/train_small/small_captions_train.txt", transform)
     small_val_dataset = ImageCaptionDataset("../data/val_small", "../data/val_small/small_captions_val.txt", transform)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    # Dataloaders
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False)
     val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-    small_train_dataloader = DataLoader(small_train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    small_train_dataloader = DataLoader(small_train_dataset, batch_size=BATCH_SIZE, shuffle=False)
     small_val_dataloader = DataLoader(small_val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-
+    
     return train_dataloader, val_dataloader, test_dataloader, small_train_dataloader, small_val_dataloader
--- utils/dataset.py
+++ utils/dataset.py
@@ -10,6 +10,9 @@ import os
 class ImageCaptionDataset(Dataset):
     def __init__(self, image_folder, captions_file, transform=None):
 
+        self.image_folder = image_folder
+        self.transform = transform
+
         root_img, root_cpt = Path(image_folder), Path(captions_file)
         if not (root_img.exists() and root_img.is_dir()):
             raise ValueError(f"Data root '{root_img}' is invalid")
@@ -17,9 +20,7 @@ class ImageCaptionDataset(Dataset):
         if not (root_cpt.exists() and root_cpt.is_file()):
             raise ValueError(f"Data root '{root_cpt}' is invalid")
 
-        self.image_folder = image_folder
-        self.transform = transform
-        self.df = pd.read_csv(captions_file, sep=",", header=None, names=["image", "caption"])
+        self.df = pd.read_table(captions_file, sep=",", header=None, names=["image", "caption"], dtype='str')
         self.img_groups = self.df.groupby("image")["caption"].apply(list).reset_index()
         
     def __len__(self):
@@ -27,47 +28,10 @@ class ImageCaptionDataset(Dataset):
     
     def __getitem__(self, idx):
         img_name = os.path.join(self.image_folder, self.img_groups.iloc[idx, 0])
-        print(img_name)
         image = Image.open(img_name)
         captions = self.img_groups.iloc[idx, 1]
         
         if self.transform: image = self.transform(image)
         
-        return image, captions
+        return image, captions, img_name
 
-
-def display_image(axis, image_tensor):
-
-    if not isinstance(image_tensor, torch.Tensor):
-        raise TypeError(
-            "The `display_image` function expects a `torch.Tensor` "
-            + "use the `ToTensor` transformation to convert the images to tensors."
-        )
-
-    image_data = image_tensor.permute(1, 2, 0).numpy()
-    height, width, _ = image_data.shape
-    axis.imshow(image_data)
-    axis.set_xlim(0, width)
-    axis.set_ylim(height, 0)
-
-
-# Show the images after applying the transformations
-def compare_transforms(transformations, index):
-
-    if not all(isinstance(transf, Dataset) for transf in transformations):
-        raise TypeError(
-            "All elements in the `transformations` list need to be of type Dataset"
-        )
-
-    num_transformations = len(transformations)
-    fig, axes = plt.subplots(1, num_transformations)
-
-    if num_transformations == 1:
-        axes = [axes]
-
-    for counter, (axis, transf) in enumerate(zip(axes, transformations)):
-        axis.set_title(f"transf: {counter}")
-        image_tensor = transf[index][0]
-        display_image(axis, image_tensor)
-
-    plt.show()
